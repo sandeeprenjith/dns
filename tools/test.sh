11bb44178911d590b0f999b746bdf9c241692915
Sandeep Renjith <sandeep.renjith@gmail.com>
2019-01-17 07:09:42 +0530

    Linked RFCs

---
+++ tools/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+wget https://tools.ietf.org/html/rfc1034
+wget https://tools.ietf.org/html/rfc1035
+wget https://tools.ietf.org/html/rfc5936
+wget https://tools.ietf.org/html/rfc1995
+wget https://tools.ietf.org/html/rfc1996
+wget https://tools.ietf.org/html/rfc7719
+wget https://tools.ietf.org/html/rfc3912
+wget https://tools.ietf.org/html/rfc2182
+wget https://tools.ietf.org/html/rfc2317
+wget https://tools.ietf.org/html/rfc2782
+wget https://tools.ietf.org/html/rfc4592
+wget https://tools.ietf.org/html/rfc2308
+wget https://tools.ietf.org/html/rfc2181
+wget https://tools.ietf.org/html/rfc6891
+wget https://tools.ietf.org/html/rfc4697
